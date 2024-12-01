8d317b3c8694c50ffbd78152c4db7654f6811f59
D3rFuch5 <fuchs_UniPA@outlook.de>
2024-12-01 14:55:28 +0100

    Updade EXE-Skript

--- kaMin_Erstellung_EXE.py
+++ kaMin_Erstellung_EXE.py
@@ -3,4 +3,4 @@ import subprocess
 #Installation Pyinstaller
 subprocess.run(['pip', 'install','--upgrade', 'pyinstaller'])
 # Erstellung EXE
-subprocess.run("pyinstaller --noconfirm --windowed --onefile --distpath ./kaMin-Die_k-Means-App --add-data Grafiken/*;Grafiken/ --name kaMin-Die_k-Means-App-Alpha_1_2 --icon Grafiken/logo_k_means.png starte_kaMin.py")
+subprocess.run("pyinstaller --noconfirm --windowed --onefile --distpath ./kaMin-Die_k-Means-App --add-data Grafiken/*:Grafiken/ --name kaMin-Die_k-Means-App-Alpha_1_2 --icon Grafiken/logo_k_means.png starte_kaMin.py")
